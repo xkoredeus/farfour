6acf79424b35791a72281fe20a6bdf61d981eff5
Sergey Nacharov <nacharovsa@it2g.ru>
2021-09-14 18:47:33 +0400

    contacts/services

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -1,3 +1,4 @@
+
 $(() => {
     var geojson = {
         "type": "FeatureCollection",
@@ -10,7 +11,7 @@ $(() => {
                 "geometry": {
                     "type": "Point",
                     "coordinates": [
-                        49.45480118878082, 53.51478732275671
+                        60.656284, 56.833903
                     ]
                 }
             }
@@ -19,21 +20,24 @@ $(() => {
 
     mapboxgl.accessToken = 'pk.eyJ1IjoieGtvcmVkZXVzIiwiYSI6ImNrbGtxcmoybzNmMGIydnM4MWs5ZXo5cmMifQ._YIlgXTnQtUtNubeZg81dg';
 
-    if ( $(window).width() > 996 ) {
-        centerPosition = [
-            49.460774459957214, 53.51445059247407
-        ]
-    } else {
-        centerPosition = [
-            49.456751254551605, 53.51467787844254
-        ];
-    }
+    centerPosition = [
+        60.656284, 56.833903
+    ];
+    // if ( $(window).width() > 996 ) {
+    //     centerPosition = [
+    //         49.460774459957214, 53.51445059247407
+    //     ]
+    // } else {
+    //     centerPosition = [
+    //         49.456751254551605, 53.51467787844254
+    //     ];
+    // }
 
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/xkoredeus/cklkrgmef0qsx17mlzyqyo3ga',
         center: centerPosition,
-        zoom: 15,
+        zoom: 14,
         attributionControl: false
     });
 
@@ -42,8 +46,7 @@ $(() => {
         // create a DOM element for the marker
         var el = document.createElement('div');
         el.className = 'marker';
-        el.style.backgroundImage =
-            'url(assets/img/balloon.svg)';
+        el.style.backgroundImage = '';
         el.style.width = marker.properties.iconSize[0] + 'px';
         el.style.height = marker.properties.iconSize[1] + 'px';
 
